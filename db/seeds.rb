54368710dee5b43304fa2efd5fea7c5b91807982
AEH <escamilla.a@gmail.com>
2015-08-31 16:39:27 -0500

    assignment 32 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 require 'faker'
 
+posts_index = 0
+posts_flag = true
+comments_index = 0
+comments_flag = true
+
 50.times do
   Post.create!(
     title:  Faker::Lorem.sentence,
@@ -8,12 +13,46 @@ require 'faker'
 end
 posts = Post.all
 
+while posts_index < posts.length
+  if (posts[posts_index].title == "Unique title") && (posts[posts_index].body == "Unique body")
+    posts_flag = false
+    break
+  else
+    posts_index += 1
+  end
+end
+
+if posts_flag
+  Post.create!(
+    title:  "Unique title",
+    body:   "Unique body"
+  )  
+end
+
 
 100.times do
   Comment.create!(
     post: posts.sample,
     body: Faker::Lorem.paragraph
   )
+end
+
+comments = Comment.all
+
+while comments_index < comments.length
+  if (comments[comments_index].body == "Unique comment")
+    comments_flag = false
+    break
+  else
+    comments_index += 1
+  end
+end
+
+if comments_flag
+  Comment.create!(
+    post:  posts.sample,
+    body:   "Unique comment"
+  )  
 end
 
 puts "Seed finished"

57909f48d76e0d9b23d38b828ca996d98a8f159f
AEH <escamilla.a@gmail.com>
2015-09-01 13:07:13 -0500

    assignment 33 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,15 @@ require 'faker'
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
   )
+
+  Advertisement.create!(
+    title:  Faker::Lorem.sentence,
+    copy:   Faker::Lorem.sentence,
+    price:  Faker::Commerce.price
+  )
 end
 posts = Post.all
+ads = Advertisement.all
 
 
 100.times do
@@ -19,3 +26,4 @@ end
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} ads created"

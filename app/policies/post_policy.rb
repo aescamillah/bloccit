3b2956555be76ba674083f4ff4bd6045098fff85
AEH <escamilla.a@gmail.com>
2015-09-03 15:10:15 -0500

    assignment 39 completed

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,4 +1,28 @@
 class PostPolicy < ApplicationPolicy
+
+  class Scope
+
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.present?
+        if @user.admin? || user.moderator?
+          scope.all
+        else
+          scope.where(:user_id => user.id)
+        end
+      else
+        scope.where ("1 = 0")
+      end
+    end
+
+  end
+
   def index?
     true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,43 +1,5 @@
 require 'faker'
 
- # Create Users
- 5.times do
-   user = User.new(
-     name:     Faker::Name.name,
-     email:    Faker::Internet.email,
-     password: Faker::Lorem.characters(10)
-   )
-   user.skip_confirmation!
-   user.save!
- end
- users = User.all
- 
- # Note: by calling `User.new` instead of `create`,
- # we create an instance of User which isn't immediately saved to the database.
- 
- # The `skip_confirmation!` method sets the `confirmed_at` attribute
- # to avoid triggering an confirmation email when the User is saved.
- 
- # The `save` method then saves this User to the database.
-
-50.times do
-  Post.create!(
-    user:   users.sample,
-    title:  Faker::Lorem.sentence,
-    body:   Faker::Lorem.paragraph
-  )
-end
-posts = Post.all
-
-
-100.times do
-  Comment.create!(
-    # user: users.sample,   # we have not yet associated Users with Comments
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
-  )
-end
-
 # Create an admin user
 admin = User.new(
   name:     'Admin User',
@@ -58,7 +20,7 @@ moderator = User.new(
 moderator.skip_confirmation!
 moderator.save!
 
-# Create a member
+# Create 2 member
 member = User.new(
   name:     'Member User',
   email:    'member@example.com',
@@ -66,6 +28,34 @@ member = User.new(
 )
 member.skip_confirmation!
 member.save!
+
+member_2 = User.new(
+  name:     'Member User 2',
+  email:    'member2@example.com',
+  password: 'helloworld'
+)
+member_2.skip_confirmation!
+member_2.save!
+
+users = User.all
+
+50.times do
+  Post.create!(
+    user:   users.sample,
+    title:  Faker::Lorem.sentence,
+    body:   Faker::Lorem.paragraph
+  )
+end
+posts = Post.all
+
+
+100.times do
+  Comment.create!(
+    # user: users.sample,   # we have not yet associated Users with Comments
+    post: posts.sample,
+    body: Faker::Lorem.paragraph
+  )
+end
 
 puts "Seed finished"
 puts "#{User.count} users created"

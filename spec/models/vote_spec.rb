2b663ba9e2e1c0e4234dbacf6d8c17273be8d07d
AEH <escamilla.a@gmail.com>
2015-10-05 15:00:54 -0500

    assignment 48 fixed

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,7 +2,11 @@ describe Vote do
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        expect(test_vote.valid?).to eq(false)
+        user = User.create
+        topic = Topic.create
+        @post = Post.create(title: 'post title', body: 'Post bodies must be pretty long.')
+        my_vote = @post.votes.create(value: 0)
+        expect(my_vote.valid?).to eq(false)
       end
     end
   end

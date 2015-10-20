dff32c9abc3e9c0e8bc90fab661fd3b11dc62b48
AEH <escamilla.a@gmail.com>
2015-10-20 16:17:52 -0500

    assignment 60 completed

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -4,5 +4,11 @@ FactoryGirl.define do
     body "Post bodies must be pretty long."
     user
     topic { Topic.create(name: 'Topic name') }
+
+    factory :post_with_comment do
+      after(:create) do |post|
+        create(:comment, post: post, user: post.user)
+      end
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,17 +27,14 @@ describe User do
   describe ".top_rated" do
 
     before do
-      @user1 = create(:user)
-      post = create(:post, user: @user1)
-      create(:comment, user: @user1, post: post)
-
+      @user1 = create(:user_with_post_and_comment)
       @user2 = create(:user)
-      post = create(:post, user: @user2)
-      2.times { create(:comment, user: @user2, post: post) }
+       post = create(:post, user: @user2)
+       2.times { create(:comment, user: @user2, post: post) }
     end
 
     it "returns users ordered by comments + posts" do
-      expect( User.top_rated ).to eq([@user2, @user1])
+      expect( User.top_rated ).to eq([@user2,@user1])
     end
 
     it "stores a `posts_count` on user" do
